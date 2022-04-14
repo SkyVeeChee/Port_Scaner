6778df4cf0cbe381510fbca7ddcb2be7af58bb6f
SkyVeeChee <82585066+SkyVeeChee@users.noreply.github.com>
2022-04-15 00:00:47 +0500

    Add files via upload

---
+++ TCPPortTracker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Linq;
+
+namespace TCPPortTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(' ')
+                .Select(x => int.Parse(x))
+                .ToArray();
+            PortScan(input[0], input[1]);
+        }
+
+        private static void PortScan(int start, int finish)
+        {
+            for (int i = start; i <= finish; i++)
+            {
+                using (TcpClient Scan = new TcpClient())
+                {
+                    try
+                    {
+                        Scan.Connect("127.0.0.1", i);
+                        Console.WriteLine(i);
+                    }
+                    catch
+                    {
+                        //Console.WriteLine("");
+                    }
+                }
+            }
+        }
+    }
+}
